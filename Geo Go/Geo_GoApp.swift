534b09e9ec3a2411e1be5d53cd8e0ac0d4c7549e
jaloliddin99 <jaloliddin49@gmail.com>
2024-06-21 15:19:17 +0500

    Initial Commit

---
+++ Geo Go/Geo_GoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Geo_GoApp.swift
+//  Geo Go
+//
+//  Created by Jaloliddin Abdullaev on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Geo_GoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
