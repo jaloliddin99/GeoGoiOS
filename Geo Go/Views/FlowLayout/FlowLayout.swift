56e67fc4cc47d5beb07677c2ae66bf9851139a0a
jaloliddin99 <jaloliddinabdullaev49@gmail.com>
2024-06-27 15:05:13 +0500

    add name and enter phone screens are finished

---
+++ Geo Go/Views/FlowLayout/FlowLayout.swift
@@ -0,0 +1,18 @@
+//
+//  FlowLayout.swift
+//  Geo Go
+//
+//  Created by Jaloliddin Abdullaev on 26/06/24.
+//
+
+import SwiftUI
+
+struct FlowLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlowLayout()
+}
