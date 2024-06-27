9a5e2fe9d458857c8ef02e18d00f8f5fc1fa42a6
jaloliddin99 <jaloliddinabdullaev49@gmail.com>
2024-06-27 18:04:55 +0500

    mapbox sdk added

---
+++ Geo Go/Screens/Main/Home/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  Geo Go
+//
+//  Created by Jaloliddin Abdullaev on 27/06/24.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
