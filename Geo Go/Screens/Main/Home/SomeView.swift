ea838aa90642f0fb578f0483c21ced98949b2914
jaloliddin99 <jaloliddinabdullaev49@gmail.com>
2024-06-28 16:05:29 +0500

    some view file added

---
+++ Geo Go/Screens/Main/Home/SomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SomeView.swift
+//  Geo Go
+//
+//  Created by Jaloliddin Abdullaev on 28/06/24.
+//
+
+import SwiftUI
+
+struct SomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SomeView()
+}
